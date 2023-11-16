58bf7faf93c8bc5ae75bd5d765c5fa714ce02fca
Felippe Fernandes <felippe1000@hotmail.com>
2023-11-16 09:04:09 -0300

    change main grid to display grid

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.main`
-  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
 `;
 
 export const ProductGrid = styled.div`
   display: grid;
+  grid-area: 2 / 2 / 8 / 8;
   grid-column-gap: 1.5rem;
   grid-row-gap: 2rem;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(2, 1fr);
   justify-items: center;
-  margin: 5rem 6rem;
 `;
